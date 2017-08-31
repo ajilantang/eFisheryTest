07417d74b1960ecc12a310e094aa2702e067aea3
ajilantang <ajilantang@gmail.com>
2017-08-31 15:37:04 +0700

    add wr code scanner

--- app/src/main/java/ajilantang/wificonnect/QrCode.java
+++ app/src/main/java/ajilantang/wificonnect/QrCode.java
@@ -1,8 +1,18 @@
 package ajilantang.wificonnect;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+
+import info.vividcode.android.zxing.CaptureActivity;
+import info.vividcode.android.zxing.CaptureActivityIntents;
 
 /**
  * Created by ajilantang on 30/08/17.
@@ -11,9 +21,38 @@ import android.widget.TextView;
 public class QrCode extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_code);
+        Button btScan = (Button) findViewById(R.id.bt_scan);
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-        TextView textview = new TextView(this);
-        textview.setText("This is BlackBerry tab");
-        setContentView(textview);
+                // Membuat intent baru untuk memanggil CaptureActivity bawaan ZXing
+                Intent captureIntent = new Intent(QrCode.this, CaptureActivity.class);
+
+                // Kemudian kita mengeset pesan yang akan ditampilkan ke user saat menjalankan QRCode scanning
+                CaptureActivityIntents.setPromptMessage(captureIntent, "Barcode scanning...");
+
+                // Melakukan startActivityForResult, untuk menangkap balikan hasil dari QR Code scanning
+                startActivityForResult(captureIntent, 0);
+            }
+        });
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TextView tvScanResult = null;
+
+        if (requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                String value = data.getStringExtra("SCAN_RESULT");
+                Log.d(value, "onActivityResult: ");
+                Toast.makeText(QrCode.this,"Wifi SSID  : "+value,Toast.LENGTH_SHORT).show();
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                Toast.makeText(QrCode.this,"failed to scan try again later",Toast.LENGTH_SHORT).show();
+            }
+        } else {
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
